ee25be43d745fdc882fbd60df340f42f2d3bac0a
yago-codev <yagomilano92@gmail.com>
2021-07-25 01:32:43 -0300

    adiciona stylesheet para estilização de componentes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,60 @@
-import React, {Fragment} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
   return (
-    <Fragment>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>React Native</Text>
-        <Text>Ignite</Text>
-      </View>
-    </Fragment>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Yago</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#c3c3c3"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, {marginTop: 20}]}>My Skills</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
