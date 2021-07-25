c8601bc205aadf12abd1db9537381fb46b0b163b
yago-codev <yagomilano92@gmail.com>
2021-07-25 02:13:32 -0300

    adiciona o hook useState para lidar com imutabilidade de estado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -9,6 +9,13 @@ import {
 } from 'react-native';
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Yago</Text>
@@ -16,11 +23,20 @@ export function Home() {
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#c3c3c3"
+        onChangeText={setNewSkill}
       />
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={handleAddNewSkill}>
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
-      <Text style={[styles.title, {marginTop: 20}]}>My Skills</Text>
+      <Text style={[styles.title, {marginVertical: 20}]}>My Skills</Text>
+      {mySkills.map((skill, index) => (
+        <TouchableOpacity style={styles.buttonSkill} key={index}>
+          <Text style={styles.textSkill}>{skill}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
@@ -55,6 +71,18 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#fff',
     fontSize: 17,
+    fontWeight: 'bold',
+  },
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
